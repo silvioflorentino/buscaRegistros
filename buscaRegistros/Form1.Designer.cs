bb96a62aa20364a0359a8849ea5dda2096ca5932
silvioflorentino <professorsilvioflorentino@yahoo.com.br>
2024-08-27 15:59:19 -0300

    Adicionar arquivos de projeto.

---
+++ buscaRegistros/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace buscaRegistros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxTemporadas = new System.Windows.Forms.ComboBox();
+            this.comboBoxEpisodios = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxTemporadas
+            // 
+            this.comboBoxTemporadas.FormattingEnabled = true;
+            this.comboBoxTemporadas.Location = new System.Drawing.Point(166, 138);
+            this.comboBoxTemporadas.Name = "comboBoxTemporadas";
+            this.comboBoxTemporadas.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxTemporadas.TabIndex = 0;
+            // 
+            // comboBoxEpisodios
+            // 
+            this.comboBoxEpisodios.FormattingEnabled = true;
+            this.comboBoxEpisodios.Location = new System.Drawing.Point(402, 138);
+            this.comboBoxEpisodios.Name = "comboBoxEpisodios";
+            this.comboBoxEpisodios.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxEpisodios.TabIndex = 1;
+            this.comboBoxTemporadas.SelectedIndexChanged += new System.EventHandler(this.comboBoxTemporadas_SelectedIndexChanged);
+
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxEpisodios);
+            this.Controls.Add(this.comboBoxTemporadas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxTemporadas;
+        private System.Windows.Forms.ComboBox comboBoxEpisodios;
+    }
+}
+
---
+++ buscaRegistros/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buscaRegistros
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, List<string>> temporadasEpisodios;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Inicializa os dados no dicionário
+            InicializarDados();
+            // Popula o ComboBox de temporadas
+            comboBoxTemporadas.Items.AddRange(temporadasEpisodios.Keys.ToArray());
+        }
+
+        private void InicializarDados()
+        {
+            // Inicializa o dicionário com temporadas e seus episódios
+            temporadasEpisodios = new Dictionary<string, List<string>>
+            {
+                { "Temporada 1", new List<string> { "Episódio 1", "Episódio 2", "Episódio 3" } },
+                { "Temporada 2", new List<string> { "Episódio 21", "Episódio 22", "Episódio 23" } },
+                { "Temporada 3", new List<string> { "Episódio 31", "Episódio 32", "Episódio 33" } }
+            };
+        }
+
+        private void comboBoxTemporadas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Limpa o ComboBox de episódios sempre que uma nova temporada for selecionada
+            comboBoxEpisodios.Items.Clear();
+            comboBoxEpisodios.Text = "";
+
+            // Obtém a temporada selecionada
+            string temporadaSelecionada = comboBoxTemporadas.SelectedItem.ToString();
+
+            // Verifica se a temporada selecionada existe no dicionário e preenche o ComboBox de episódios
+            if (temporadasEpisodios.ContainsKey(temporadaSelecionada))
+            {
+                comboBoxEpisodios.Items.AddRange(temporadasEpisodios[temporadaSelecionada].ToArray());
+            }
+        }
+    }
+}
